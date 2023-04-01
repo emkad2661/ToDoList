2465bb259b79f5712e88ee4568d4e975d5cbf5ca
Faik Emre Kadısoyu <emrekadisoyu@hotmail.com>
2023-04-01 17:07:31 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Faik Emre Kadısoyu on 1.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
